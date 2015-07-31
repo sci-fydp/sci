0061578cc5d4924d4818f85c755e60f43daef811
Eric Gwangseung Kim <gwangseung@hotmail.com>
2015-07-30 21:51:39 -0400

    Initial commit for trend analysis

---
+++ store-router/app/controllers/ShoppingList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class ShoppingList {
+	public int date;
+	public ArrayList<String> items;
+
+	public ShoppingList(int d) {
+		date = d;
+		items = new ArrayList<String>();
+	}
+
+	public int getDate() {
+		return date;
+	}
+
+	public ArrayList<String> getList() {
+		return items;
+	}
+}
